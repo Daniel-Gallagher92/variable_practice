d30b6d7454e26fffe1ca81d625f224e194cfd225
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-04-11 14:15:10 -0600

    Fixes issue with concatonating different variable types

--- variables.rb
+++ variables.rb
@@ -1,19 +1,19 @@
 email = "Daniel.Gallagher92@gmail.com"
 username = "Daniel-Gallagher92"
-state_of_mind = "is happy"
-state_of_employment = "is going to quit his job and jump into a whole new world"
+state_of_mind = " is happy"
+state_of_employment = " is going to quit his job and jump into a whole new world"
 
 is_daniel_happy = "Daniel practices mindfulness and is happy"
-turing_balance_owed = "Daniel owes Turing"
+turing_balance_owed = "Daniel owes Turing 23,800"
 
 
 small_group = 07
 cohort = 2305
 
-specific_starting_point = "Daniel, Parker, Allen and Dylan all started with #{cohort} and were all placed together in group #{small_group}"
+specific_starting_point = "Daniel, Parker, Allen and Dylan all started with cohort #{cohort} and were all placed together in group #{small_group}"
 
 balance = 1.25
-owed = 23,800.00
+owed = 23800.00
 
 rich = false
 happy = true
@@ -24,5 +24,6 @@ username = "DanielG"
 puts username + state_of_mind
 puts username + state_of_employment
 puts specific_starting_point
-puts is_daniel_happy + is_happy
-puts turing_balance_owed + balance_owed
+puts is_daniel_happy 
+puts turing_balance_owed 
+puts email
