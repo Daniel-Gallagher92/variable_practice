277128347750a81144840ce42027fefb3c9dfc22
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-04-11 13:21:26 -0600

    Add variables and puts statements

--- variables.rb
+++ variables.rb
@@ -1,11 +1,26 @@
 email = "Daniel.Gallagher92@gmail.com"
 username = "Daniel-Gallagher92"
+state_of_mind = "is happy"
+state_of_employment = "is going to quit his job and jump into a whole new world"
+specific_starting_point = "Daniel, Parker, Allen and Dylan all started with #{cohort} and were all placed together in group #{small_group}"
+is_daniel_happy = "Daniel practices mindfulness and is happy"
+turing_balance_owed = "Daniel owes Turing"
+
 
 small_group = 07
 cohort = 2305
 
 balance = 1.25
-gpa 4.0
+balance_owed = 23,800.00
 
 is_rich = false
 is_happy = true
+
+email = "Doot.doot@dootmail.doot"
+username = "DanielG"
+
+puts username + state_of_mind
+puts username + state_of_employment
+puts specific_starting_point
+puts is_daniel_happy + is_happy
+puts turing_balance_owed + balance_owed
